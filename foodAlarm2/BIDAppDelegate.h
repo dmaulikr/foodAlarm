361ad255638a5d3212f448b0103cd1b9af3b2cc3
Remm1 <mremick@gmail.com>
2013-05-16 12:26:16 -0700

    Initial Commit

---
+++ foodAlarm2/BIDAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  BIDAppDelegate.h
+//  foodAlarm2
+//
+//  Created by Matt Remick on 5/16/13.
+//  Copyright (c) 2013 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
