1d7ad781b7aecdc17ca5f09e8d5722f09552b237
Remm1 <mremick@gmail.com>
2013-09-13 12:40:48 -0700

    first commit

---
+++ BIDAlarmViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDAlarmViewController.h
+//  foodAlarm2
+//
+//  Created by Matt Remick on 5/16/13.
+//  Copyright (c) 2013 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAlarmViewController : UIViewController
+
+@end
---
+++ foodAlarm2/BIDFoodViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDFoodViewController.h
+//  foodAlarm2
+//
+//  Created by Matt Remick on 5/16/13.
+//  Copyright (c) 2013 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDFoodViewController : UIViewController
+
+@end
